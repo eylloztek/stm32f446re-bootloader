9c04f278aae4fe22a796ff0a52a4c40e367ccbc3
eylloztek <eyluloztek2@gmail.com>
2026-07-04 00:15:42 +0300

    defined addresses

--- Bootloader/Core/Inc/bootloader.h
+++ Bootloader/Core/Inc/bootloader.h
@@ -9,7 +9,28 @@
 #define INC_BOOTLOADER_H_
 
 #include "main.h"
+#include "string.h"
+
+#define BOOTLOADER_HEADER 				0x7F
+#define APPLICATION_HEADER				0x7E
+#define BOOTLOADER_VERSION				0x10
+
+#define GET_HELP						0x00
+#define GET_VERSION						0x01
+#define GET_ID							0x02
+#define READ_MEMORY						0x11
+#define GO_TO_ADDRESS					0x21
+#define WRITE_MEMORY					0x31
+#define ERASE							0x43
+#define WRITE_PROTECT_UNPROTECT			0x63
+#define READOUT_PROTECT_UNPROTECT		0x82
+#define GET_CHECKSUM					0xA1
+
+#define ACK								0x79
+#define NACK							0x1F
+#define UNKNOWN							0x99
 
 void processBootloaderCommand(void);
+void handleGetVersion(void);
 
 #endif /* INC_BOOTLOADER_H_ */
--- Bootloader/Core/Src/bootloader.c
+++ Bootloader/Core/Src/bootloader.c
@@ -7,6 +7,24 @@
 
 #include "bootloader.h"
 
+extern char messageBuffer[BUFFER_SIZE];
+extern uint8_t bufferIndex;
+
 void processBootloaderCommand(void){
+	uint8_t command = messageBuffer[2];
+
+	switch (command) {
+		case GET_VERSION:
+			handleGetVersion();
+			break;
+		default:
+			break;
+	}
+
+	bufferIndex = 0;
+	memset(messageBuffer, 0, BUFFER_SIZE);
+}
+
+void handleGetVersion(void){
 
 }
