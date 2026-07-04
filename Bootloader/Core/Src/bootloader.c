b0690e1c9d70e66d87f3e22e93871691bc626bf4
eylloztek <eyluloztek2@gmail.com>
2026-07-05 00:12:39 +0300

    getVersion function

--- Bootloader/Core/Src/bootloader.c
+++ Bootloader/Core/Src/bootloader.c
@@ -10,21 +10,35 @@
 extern char messageBuffer[BUFFER_SIZE];
 extern uint8_t bufferIndex;
 
-void processBootloaderCommand(void){
+void processBootloaderCommand(void) {
 	uint8_t command = messageBuffer[2];
 
 	switch (command) {
-		case GET_VERSION:
-			handleGetVersion();
-			break;
-		default:
-			break;
+	case GET_VERSION:
+		handleGetVersion();
+		break;
+	default:
+		break;
 	}
 
 	bufferIndex = 0;
 	memset(messageBuffer, 0, BUFFER_SIZE);
 }
 
-void handleGetVersion(void){
+void handleGetVersion(void) {
+	uint8_t response[2] = { 0 };
+
+	if (BOOTLOADER_VERSION > 0 && BOOTLOADER_VERSION <= 255) {
+		response[0] = ACK;
+		response[1] = BOOTLOADER_VERSION;
+	} else {
+		response[0] = NACK;
+		response[1] = UNKNOWN;
+	}
+
+#ifdef DEBUG_PRINT
+	printf("Bootloader Version: 0x%x \r\n", BOOTLOADER_VERSION);
+#endif
+	HAL_UART_Transmit(UART_PORT, response, sizeof(response), HAL_MAX_DELAY);
 
 }
